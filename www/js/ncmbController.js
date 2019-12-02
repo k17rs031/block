23726e56be4792219a9f9530984c70fb2aaa8469
k17rs031 <k17rs031@st.kyusan-u.ac.jp>
2019-12-02 14:17:30 +0900

    ユーザー登録処理の追加、編集

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -98,6 +98,19 @@ updateDisplayName: function(){
     return this.currentUser.update();
 },
 
+  uuid: function uuid() {
+  var uuid = "", i, random;
+  for (i = 0; i < 32; i++) {
+    random = Math.random() * 16 | 0;
+
+    if (i == 8 || i == 12 || i == 16 || i == 20) {
+      uuid += "-"
+    }
+    uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+  }
+  return uuid;
+},
+
   //初期化
   init: function(screenSize){
     var self = this;
